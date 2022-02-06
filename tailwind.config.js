870d30ad2f36c284f45427fe215f975e3335602f
dneri <d.neri@zerynth.com>
2022-02-06 18:59:56 +0100

    index

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
       "./src/**/*.{html,js}",
-     './public/index.html',
+     './index.html',
   ],
   theme: {
     extend: {
