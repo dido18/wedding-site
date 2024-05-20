3bea3444c7f44f3b3a4c1c8d2434c90e2b68d448
Davide N <davideneri18@gmail.com>
2024-05-20 21:20:48 +0200

    change color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,20 +10,6 @@ module.exports = {
       fontFamily: {
         'sans': ['Fondamento', ...defaultTheme.fontFamily.sans]
       },
-      transitionDuration: {
-        '0': '0ms',
-        '2000': '2000ms',
-        '2500': '2500ms',
-        '4000': '4000ms',
-        '6000': '6000ms',
-      },
-      transitionDelay: {
-        '1500': '1500ms',
-        '2000': '2000ms',
-        '2500': '2500ms',
-        '3000': '3000ms',
-        '6000': '6000ms',
-      }
     },
   },
   plugins: [],
