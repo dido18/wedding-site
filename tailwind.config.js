3ca3b71e4b53e94acb16bd3bd10a42bb4f2821d3
Davide N <davideneri18@gmail.com>
2024-07-17 23:19:49 +0200

    change color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,18 @@ module.exports = {
     colors: {
         'brown': '#f1f1ea',
         'brown-900': '#3a210c',
+        'blue-900': '#264681',
+        'blue-700': '#5e83ad',
+        'blue-200': '#c6d1dc',
     },
     extend: {
       fontFamily: {
         'sans': ['Fondamento', ...defaultTheme.fontFamily.sans]
       },
     },
-    safelist: [
-        'bg-orange-50',
-      ],
+    // safelist: [
+    //     'bg-orange-50',
+    //   ],
   },
   plugins: [],
 }
